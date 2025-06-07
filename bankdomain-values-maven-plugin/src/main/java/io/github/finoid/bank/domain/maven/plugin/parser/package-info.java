45803f967e246926bbdcbb3634e46a92e310c1f9
Nicklas Wallgren <nicklas.wallgren@gmail.com>
2025-06-07 09:27:58 +0200

    chore: use resource-based CSV handling and `@NullMarked` annotations across packages.

---
+++ bankdomain-values-maven-plugin/src/main/java/io/github/finoid/bank/domain/maven/plugin/parser/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package io.github.finoid.bank.domain.maven.plugin.parser;
+
+import org.jspecify.annotations.NullMarked;
---
+++ bankdomain-values-maven-plugin/src/main/java/io/github/finoid/bank/domain/maven/plugin/util/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package io.github.finoid.bank.domain.maven.plugin.util;
+
+import org.jspecify.annotations.NullMarked;
